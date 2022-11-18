baf97db1b13d460a043c13ace3939e6de91f2643
maulana48 <hidarizero4869@gmail.com>
2022-11-18 11:33:32 +0700

    terakhir(mungkin?)

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,22 +26,3 @@ module.exports = {
     }
 
 }
-
-tailwind.config = {
-    theme: {
-        fontFamily: {
-            'sans': ['Inter'],
-        },
-        container: {
-            center: true,
-            screens: {
-                xl: '1170px',
-            },
-        },
-        extend: {
-            colors: {
-                clifford: '#da373d',
-            },
-        },
-    }
-}
